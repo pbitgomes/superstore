481e1057026aca053c262c549d6643273b44f76e
Paula Bittencourt <paula.bittencourt@gmail.com>
2022-12-02 10:59:19 -0300

    schemas

--- models/orders.model.js
+++ models/orders.model.js
@@ -1,3 +1,32 @@
-// schemas
-// cria o modelo
+import { model, Schema } from "mongoose";
+
+// criar o schema
+const orderSchema = new Schema({
+  // products: [
+  //     {
+  //      product: ObjectId("jdfvlbadkvjdflkjvb"),
+  //      amount: 10
+  //     }
+  // ]
+  products: [
+    {
+      product: {
+        type: Schema.Types.ObjectId,
+        ref: "Product",
+      },
+      amount: {
+        type: Number,
+      },
+    },
+  ],
+  salesDate: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// criar o modelo
+const OrderModel = model("Order", orderSchema);
+
 // exportar para as rotas
+export default OrderModel;
--- models/products.model.js
+++ models/products.model.js
@@ -1,3 +1,34 @@
+import { model, Schema } from "mongoose";
+
+// criar o schema, criar o modelo (model) e exportar para as rotas
+
 // schemas
-// cria o modelo
+const productSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+  },
+  category: {
+    type: String,
+    enum: ["Móveis", "Plantas", "Ferramentas"],
+  },
+// orders: [
+//   objectId("123vdsfbsdfvsdf"),
+//   objectId("dnviluebvln12")
+// ]
+  orders: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "",
+    },
+  ],
+});
+
+// cria o modelo (model)
+const ProductModel = model("Product", productSchema);
+
 // exportar para as rotas
+export default ProductModel;
